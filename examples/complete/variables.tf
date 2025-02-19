a0b0c0525258488f813becbe5e131a8dae16c0b6
Igor Rodionov <goruha@gmail.com>
2025-02-19 15:22:28 +0300

    Update variables.tf

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -3,11 +3,6 @@ variable "region" {
   description = "AWS region"
 }
 
-variable "retention_in_days" {
-  description = "Number of days you want to retain log events in the log group"
-  default     = "30"
-}
-
 variable "stream_names" {
   type        = list(string)
   description = "Names of streams"
